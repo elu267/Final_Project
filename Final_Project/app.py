013f9172a0d08d7788ef937645666534d3567ea2
Emanshu Patel <epatel@MoonriseHotel.local>
2019-03-22 13:42:56 -0500

    issue with image conversion

--- Final_Project/app.py
+++ Final_Project/app.py
@@ -88,11 +88,11 @@ print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
+    img = image.load_img(img_path, target_size=(75, 100))
 
     # Preprocessing the image
     x = image.img_to_array(img)
-    x = np.true_divide(x, 255)
+   #  x = np.true_divide(x, 255)
     x = np.expand_dims(x, axis=0)
 
     # Be careful how your trained model deals with the input
